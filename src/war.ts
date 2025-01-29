0ae279b170ec70dbecfadb3d56a00e2c810b59f2
Joey Muchow <193647139+joeymuchow@users.noreply.github.com>
2025-01-28 22:08:18 -0800

    Create deck and card classes, deck of cards fn
    Classes have been created for decks and cards so that they can be
    created and assigned to players. A function for creating the deck of
    cards the game will be played with has been created too.

--- src/war.ts
+++ src/war.ts
@@ -1,23 +1,54 @@
 // Types
 type WarPlayer = {
     name: string;
-    deck: Card[];
+    deck: WarDeck;
 }
 
-type Card = {
+type WarCard = {
     suit: "clubs" | "diamonds" | "hearts" | "spades";
     value: 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14;
     name: "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "J" | "Q" | "K" | "A";
 }
 
+type WarDeck = {
+    cards: WarCard[];
+    total: number;
+}
+
 // Classes
 class Player implements WarPlayer {
     name: string;
-    deck: Card[];
+    deck: WarDeck;
 
-    constructor(name: string, deck: Card[]) {
+    constructor(name: string, deck: WarDeck) {
         this.name = name;
         this.deck = deck;
+    }
+}
+
+class Card implements WarCard {
+    suit: "clubs" | "diamonds" | "hearts" | "spades";
+    value: 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14;
+    name: "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "J" | "Q" | "K" | "A";
+
+    constructor(
+        suit: "clubs" | "diamonds" | "hearts" | "spades",
+        value: 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14,
+        name: "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "J" | "Q" | "K" | "A"
+    ) {
+        this.suit = suit;
+        this.value = value;
+        this.name = name;
+    }
+}
+
+class Deck implements WarDeck {
+    cards: WarCard[];
+    total: number;
+
+    constructor(cards: WarCard[]) {
+        this.cards = cards;
+        this.total = cards.length;
     }
 
     // Pull card from top of deck
@@ -31,17 +62,63 @@ document.querySelector(".start-game")?.addEventListener("click", () => {
 });
 
 // Game
-function startGame() {
+function startGame(): void {
+    // Function to fill main deck
+    const mainDeck = new Deck(fillMainDeck());
+    // Shuffle deck
+
+    // Deal half the cards to each player
+
+    const playerDeck = new Deck([]);
+    const computerDeck = new Deck([]);
     const playerName: string = getPlayerName();
-    const player = new Player(playerName, []);
+    const player = new Player(playerName, playerDeck);
+    const computer = new Player("Computer", computerDeck);
 
     console.log(player.name);
+    console.log(computer.name);
+    console.log(mainDeck);
 }
 
-function getPlayerName() {
+function getPlayerName(): string {
     let response = prompt("What is your name?", "");
     if (!response) {
         response = "Player";
     }
     return response;
+}
+
+function fillMainDeck(): WarCard[] {
+    const deck: WarCard[] = [];
+
+    for (let i = 0; i < 13; i++) {
+        let cardName;
+        const cardValue: WarCard["value"] = i + 2 as WarCard["value"];
+
+        switch(cardValue) {
+            case 11:
+                cardName = "J";
+                break;
+            case 12:
+                cardName = "Q";
+                break;
+            case 13:
+                cardName = "K";
+                break;
+            case 14:
+                cardName = "A";
+                break;
+            default:
+                cardName = cardValue.toString();
+        }
+
+        const club = new Card("clubs", cardValue, cardName);
+        const diamond = new Card("diamonds", cardValue, cardName);
+        const heart = new Card("hearts", cardValue, cardName);
+        const spade = new Card("spades", cardValue, cardName);
+        deck.push(club, diamond, heart, spade);
+
+    }
+
+    return deck;
 }
